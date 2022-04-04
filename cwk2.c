204e2d27a56b7530345b8e29b9cd3ced91aaa9d3
sc18jt <sc18jt@leeds.ac.uk>
2022-04-04 16:18:06 +0100

    reset

--- cwk2.c
+++ cwk2.c
@@ -65,19 +65,6 @@ int main( int argc, char *argv[] )
 
 	printf("Process %d of %d \n", rank, numprocs);
 
-	MPI_Scatter( A, rowsPerProc, MPI_INT, A_perProc, rowsPerProc, MPI_INT, 0, MPI_COMM_WORLD );
-	MPI_Scatter( b, rowsPerProc, MPI_INT, b_perProc, rowsPerProc, MPI_INT, 0, MPI_COMM_WORLD );
-	MPI_Bcast( &x, 1, MPI_INT, 0, MPI_COMM_WORLD );
-
-	int row, col;
-	for (row=0; row<rowsPerProc; row++ )
-	{
-		b[row] = 0.0f;
-		for( col=0; col<N; col++ )
-			b[row] += A[row*rowsPerProc+col] * x[col];
-	}
-
-	MPI_Gather( &b_perProc, rowsPerProc, MPI_INT, b, rowsPerProc, MPI_INT, 0, MPI_COMM_WORLD );
 
 	//
 	// Check the answer on rank 0 in serial. Also output the result of the timing.
